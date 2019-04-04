759cd26862d1f550a2f5c7e2e6d216d5b7f8ca38
darcyh13 <44274234+darcyh13@users.noreply.github.com>
2019-04-04 11:55:06 -0400

    rough

--- Projects/Vein_Epistasis/SuperVein.R
+++ Projects/Vein_Epistasis/SuperVein.R
@@ -8,39 +8,29 @@ gene1only <- subset(wings, gene2 == "wt")
 #want to see if these visual differences between background is significant due to interaction of genotype and background - this shows each genotype, which we don't care about.
 ggplot(data = gene1only, aes(x=gene1, y=supervein)) + geom_boxplot(aes(fill=background))
 
-#is there way to show total for each background 
-#add superveins for each genotype? 
-
-mutants <- gene1only %>%
-  filter (gene1 != "wt")
-
-mutants$gene1 <- "mutant"
-
-wt <- gene1only %>%
-  filter( gene1 == "wt")
-
-df <- rbind(mutants, wt)
-
-#shows gene 1 mutants grouped together - relationship we care about! 
-ggplot(data = df, aes(x=gene1, y=supervein)) + geom_boxplot(aes(fill=background))
-
-
 library(lme4)
 #look at data to see if linear looks like it should reasonable fit 
 hist(wings$supervein)
 qqnorm(wings$supervein)
 
-
 #singular fit 
-lmm1_sv <- lmer(supervein ~ background*gene1 + (1|rep), data = gene1only)
 
-#fit linear for now
-lm1_sv <- lm(supervein ~ background*gene1, data = gene1only)
+lmm1_sv <- lmer(supervein ~ background*gene1*sex + (1|rep), data = gene1only)
 
-plot(lm1_sv)
-summary(lm1_sv)
+#make model for each vein 
 
-Anova(lm1_sv)
+lmm1_L2s <- lmer(L2s ~ background*gene1*sex + (1|rep), data = gene1only)
 
-library(dotwhisker)
-dwplot(lm1_sv)
+lmm1_L3s <- lmer(L3s ~ background*gene1*sex + (1|rep), data = gene1only)
+
+#singular fit
+#lmm1_L4s <- lm(L4s ~ background*gene1*sex + (1|rep), data = gene1only)
+#fit random effect as fixed effect 
+#get the level coefficients in the sum to zero form, and then compute the standard deviation of the coefficients
+lmm1_L4s <- lm(L4s ~ background*gene1*sex + rep, data = gene1only)
+summary(lmm1_L4s)
+
+lmm1_L5s <- lmer(L5s ~ background*gene1*sex + (1|rep), data = gene1only)
+
+
+
