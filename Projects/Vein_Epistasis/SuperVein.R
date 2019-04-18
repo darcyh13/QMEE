dfd376f19a801109a8a7f6a8bcf0f99beb402083
darcyh13 <44274234+darcyh13@users.noreply.github.com>
2019-04-18 19:05:44 -0400

    Rough - with code from Wed meeting

--- Projects/Vein_Epistasis/SuperVein.R
+++ Projects/Vein_Epistasis/SuperVein.R
@@ -30,31 +30,56 @@ plot(supervein_lmm1)
 #there is deviation for expected distrubution 
 #simulation outliers are highlighted as red stars. data points that are outside the range of simulated values. 
 #lines should be straight but some deviations are normal
+#fit doesn't look aweome which is expected. 
 
 #inference
+library(car)
 Anova(lmm1_sv)
 #strong support there is sign effect of background and gene1 on supervein (This is predicted). What we care about is there looks to be significant effect of interaction of gene1 and background and background and sex. 
 
-#effects
+#visual of effects
+library(effects)
 (eff <- allEffects(lmm1_sv))
 plot(eff)
 
 
 #make model for each vein 
-
-lmm1_L2s <- lmer(L2s ~ background*gene1*sex + (1|rep), data = gene1only)
+library(lme4)
+lmm1_L2s <- glmmTMB(L4s ~ background*gene1*sex + (1|rep), data = gene1only)
 summary(lmm1_L2s)
 plot(lmm1_L2s)
 Anova(lmm1_L2s)
-#doesnt work when I add sex. Ask Katie. 
-#emmeans_test <- emmeans(lmm1_L2s,"sex" ,"gene1", "background")
-#plot(emmeans_test, comparisons = TRUE)  
+
+gene1only$gene1 <- relevel(gene1only$gene1, "wt")
+
+library(emmeans)
+emmeans_test <- emmeans(lmm1_L2s , specs = c("gene1", "background", "sex"))
+emm2 <- emmeans(lmm1_L2s, pairwise ~ background|background:gene1:sex)
+emm3 <- emmeans(lmm1_L2s, pairwise ~ background|gene1|sex)
+
+
+
+plot(emm2)
+emm2
+plot(emm3)
+emm3
+
+emmip(lmm1_L2s, sex ~ background|gene1, CIs = T)
+
+
 
 lmm1_L3s <- lmer(L3s ~ background*gene1*sex + (1|rep), data = gene1only)
 summary(lmm1_L3s)
-plot(lmm1_L2s)
-Anova(lmm1_L2s)
+plot(lmm1_L3s)
+Anova(lmm1_L3s)
 
+
+
+
+library(emmeans)
+emmeans_test <- emmeans(lmm1_L3s , specs = c("gene1", "background", "sex"))
+emmeans_test
+plot(emmeans_test, comparisons = TRUE)  
 
 lmm1_L4s <- glmmTMB(L4s ~ background*gene1*sex + (1|rep),
                   data = gene1only)
@@ -65,4 +90,8 @@ plot(L4_lmm1)
 lmm1_L5s <- lmer(L5s ~ background*gene1*sex + (1|rep), data = gene1only)
 summary(lmm1_L5s)
 plot(lmm1_L5s)
-anova(lmm1_L5s)
+Anova(lmm1_L5s)
+
+#want to show emmeans visual of two different veins showing that we shouldnt do supervein because relationships change in each vein. 
+
+
