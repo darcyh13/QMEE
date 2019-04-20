ea2cb9097cb69fc2973570d151a68a1e97e18a0d
KatiePelletier <pellek1@mcmaster.ca>
2019-04-20 10:32:04 -0400

    bootie function!

--- Projects/Vein_Epistasis/bootie function.R
+++ Projects/Vein_Epistasis/bootie function.R
@@ -1,14 +1,16 @@
-bootie <- function(dat=gene1only, genename = "Egfr" ) {
+bootie <- function(dat=gene1only, genename = "Egfr", vein = gene1only$L2s) {
   new_dat <- gene1only[sample(nrow(gene1only), nrow(gene1only), replace = T),  ]
-  
-  mod_new <- lm(L3s ~ background*sex, 
+  mod_new <- lm(vein ~ background*sex, 
                   data = new_dat,
                   subset = gene1only$gene1 == genename)
-  return(coef(mod_new)[1:2])
+  return(coef(mod_new))
 }
 
 
 coef_out <- replicate(n = 100, bootie())
 dim(coef_out)
-ratio_coef <- coef_out[1,]/coef_out[2,]
-quantile(ratio_coef, probs = c(0.025, 0.5, 0.975))
+ratio_coef_f <- coef_out[2,]/coef_out[1,]
+quantile(ratio_coef_f, probs = c(0.5, 0.5, 0.95))
+ratio_coef_m <- coef_out[4,]/coef_out[3,]
+quantile(ratio_coef_m, probs = c(0.05, 0.5, 0.95))
+
